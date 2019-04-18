7e62f8c4e968ab2890c6707c01074a488cb6a369
U-DASTUD\anishikawa <anishikawa@TUD205772.tudelft.net>
2019-04-18 18:48:28 +0200

    Added option that allows providing custom path to mucg83 executable

--- bin/mucg83_python_interface.py
+++ bin/mucg83_python_interface.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -59,85 +60,92 @@ def parse_stdout(stdout):
     df['SHEAR_CTEMP'] = df['CTEMP']
     if Bs_n:
         # assigns nan to SHEAR_CTEMP when SHEAR_CTEMP is higher than Bs
-        df['SHEAR_CTEMP'].where(cond=df['SHEAR_CTEMP'] <= Bs_n, other=np.nan, inplace=True)
+        df['SHEAR_CTEMP'].where(cond=df['SHEAR_CTEMP']
+                                <= Bs_n, other=np.nan, inplace=True)
 
     return df, Ws, Bs_g, Bs_n, Ms
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--C', type=float, default=0., help='Carbon wt.%')
-    parser.add_argument('--Si', type=float, default=0., help='Silicon wt.%')
-    parser.add_argument('--Mn', type=float, default=0., help='Manganese wt.%')
-    parser.add_argument('--Ni', type=float, default=0., help='Nickel wt.%')
-    parser.add_argument('--Mo', type=float, default=0., help='Molybdenum wt.%')
-    parser.add_argument('--Cr', type=float, default=0., help='Chromium wt.%')
-    parser.add_argument('--V', type=float, default=0., help='Vanadium wt.%')
-    parser.add_argument('--Co', type=float, default=0., help='Cobalt wt.%')
-    parser.add_argument('--Cu', type=float, default=0., help='Copper wt.%')
-    parser.add_argument('--Al', type=float, default=0., help='Aluminium wt.%')
-    parser.add_argument('--W', type=float, default=0., help='Tungsten wt.%')
-
-    # Calls mucg83
-    cmd = './mucg83'
-    proc = Popen(cmd,
-                 stdin=PIPE,
-                 stdout=PIPE,
-                 stderr=PIPE)
-
-    composition = vars(parser.parse_args())
-    args = ('1\n'
-            '{C}\n'     # Carbon wt.% ?
-            '{Si}\n'    # Silicon wt.% ?
-            '{Mn}\n'    # Manganese wt.% ?
-            '{Ni}\n'    # Nickel wt.% ?
-            '{Mo}\n'    # Molybdenum wt.% ?
-            '{Cr}\n'    # Chromium wt.% ?
-            '{V}\n'     # Vanadium wt.% ?
-            '{Co}\n'    # Cobalt wt.% ?
-            '{Cu}\n'    # Copper wt.% ?
-            '{Al}\n'    # Aluminium wt.% ?
-            '{W}\n'     # Tungsten wt.% ?
-            '0').format(**composition)
-
-    # Passes compositions to mucg83 and reads output
-    # Important information is stored in stdout
-    stdout, stderr = proc.communicate(args.encode())
+    parser = argparse.ArgumentParser(description='Python interface of MUCG83 for calculating TTT diagrams')
+    parser.add_argument('-C', '--C', type=float, default=0., help='Carbon wt.%')
+    parser.add_argument('-Si', '--Si', type=float, default=0., help='Silicon wt.%')
+    parser.add_argument('-Mn', '--Mn', type=float, default=0., help='Manganese wt.%')
+    parser.add_argument('-Ni', '--Ni', type=float, default=0., help='Nickel wt.%')
+    parser.add_argument('-Mo', '--Mo', type=float, default=0., help='Molybdenum wt.%')
+    parser.add_argument('-Cr', '--Cr', type=float, default=0., help='Chromium wt.%')
+    parser.add_argument('-V', '--V', type=float, default=0., help='Vanadium wt.%')
+    parser.add_argument('-Co', '--Co', type=float, default=0., help='Cobalt wt.%')
+    parser.add_argument('-Cu', '--Cu', type=float, default=0., help='Copper wt.%')
+    parser.add_argument('-Al', '--Al', type=float, default=0., help='Aluminium wt.%')
+    parser.add_argument('-W', '--W', type=float, default=0., help='Tungsten wt.%')
+    parser.add_argument('--cmd', default='mucg83.exe', help='Path to mucg83 executable')
 
     try:
-        proc.kill()
+        args = parser.parse_args()
+
+        # Calls mucg83
+        proc = Popen(args.cmd,
+                     stdin=PIPE,
+                     stdout=PIPE,
+                     stderr=PIPE)
+
+        composition = vars(args)
+        composition.pop('cmd')
+        options = ('1\n'
+                   '{C}\n'     # Carbon wt.% ?
+                   '{Si}\n'    # Silicon wt.% ?
+                   '{Mn}\n'    # Manganese wt.% ?
+                   '{Ni}\n'    # Nickel wt.% ?
+                   '{Mo}\n'    # Molybdenum wt.% ?
+                   '{Cr}\n'    # Chromium wt.% ?
+                   '{V}\n'     # Vanadium wt.% ?
+                   '{Co}\n'    # Cobalt wt.% ?
+                   '{Cu}\n'    # Copper wt.% ?
+                   '{Al}\n'    # Aluminium wt.% ?
+                   '{W}\n'     # Tungsten wt.% ?
+                   '0').format(**composition)
+
+        # Passes compositions to mucg83 and reads output
+        # Important information is stored in stdout
+        stdout, stderr = proc.communicate(options.encode())
+
+        try:
+            proc.kill()
+        except:
+            pass
+
+        # Parse stdout into pandas DataFrame df and critical temperatures
+        # Ws, Bs_g, Bs_n, and Ms
+        df, Ws, Bs_g, Bs_n, Ms = parse_stdout(stdout.decode())
+
+        # Plot TTT
+        fig, ax = plt.subplots()
+
+        ax.plot(df['DIFFT'], df['CTEMP'], 'k-', label='Ferrite + Pearlite')
+        ax.plot(df['SHEART'], df['SHEAR_CTEMP'], 'r-', label='Bainite')
+
+        ax.set_xscale('log')
+        ax.set_xlim(1e-2, 1e6)
+        # ax.set_ylim(100, 900)
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel(u'Temperature (°C)')
+
+        title = ['{}{}'.format(v, k) for k, v in composition.items() if v > 0]
+        title.insert(0, 'Fe')
+        ax.set_title('-'.join(title))
+
+        if Bs_n:
+            print('Bainite start temperature:', Bs_n, 'oC')
+            ax.axhline(Bs_n, color='r', ls='--')
+            ax.text(ax.get_xlim()[0]*1.5, Bs_n + 10, 'Bainite start', color='r')
+        if Ms:
+            print('Martensite start temperature:', Ms, 'oC')
+            ax.axhline(Ms, color='b', ls='--')
+            ax.text(ax.get_xlim()[0]*1.5, Ms + 10, 'Martensite start', color='b')
+
+        ax.legend()
+
+        plt.show()
     except:
         pass
-
-    # Parse stdout into pandas DataFrame df and critical temperatures
-    # Ws, Bs_g, Bs_n, and Ms
-    df, Ws, Bs_g, Bs_n, Ms = parse_stdout(stdout.decode())
-
-    # Plot TTT
-    fig, ax = plt.subplots()
-
-    ax.plot(df['DIFFT'], df['CTEMP'], 'k-', label='Ferrite + Pearlite')
-    ax.plot(df['SHEART'], df['SHEAR_CTEMP'], 'r-', label='Bainite')
-
-    ax.set_xscale('log')
-    ax.set_xlim(1e-2, 1e6)
-    # ax.set_ylim(100, 900)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel(u'Temperature (°C)')
-
-    title = ['{}{}'.format(v, k) for k, v in composition.items() if v > 0]
-    title.insert(0, 'Fe')
-    ax.set_title('-'.join(title))
-
-    if Bs_n:
-        print('Bainite start temperature:', Bs_n, 'oC')
-        ax.axhline(Bs_n, color='r', ls='--')
-        ax.text(ax.get_xlim()[0]*1.5, Bs_n + 10, 'Bainite start', color='r')
-    if Ms:
-        print('Martensite start temperature:', Ms, 'oC')
-        ax.axhline(Ms, color='b', ls='--')
-        ax.text(ax.get_xlim()[0]*1.5, Ms + 10, 'Martensite start', color='b')
-
-    ax.legend()
-
-    plt.show()
